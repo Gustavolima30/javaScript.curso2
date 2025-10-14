aff5e3d2cf83f5113b3bbf8f8dabfa9cac31cb16
Gustavolima30 <lima.munhoz@escola.pr.gov.br>
2025-10-14 11:04:13 -0300

    ok

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-oi
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNatela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = TextDecoder;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNatela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
