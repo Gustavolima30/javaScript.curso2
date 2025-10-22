d1dea6ae653951a6c3bfc53fb0202f2f29383187
Gustavolima30 <lima.munhoz@escola.pr.gov.br>
2025-10-22 08:17:22 -0300

    ok

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-let numeroSecreto = 7
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1
 
 function exibirTextoNatela(tag, texto){
     let campo = document.querySelector(tag);
-    campo.innerHTML = TextDecoder;
+    campo.innerHTML = texto;
 }
 
+function exibirMensagemInicial(){
 exibirTextoNatela('h1', 'Jogo do número secreto');
 exibirTextoNatela('p', 'Escolha um número entre 1 e 100');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
    
     if(chute == numeroSecreto) {
-        exibirTextoNatela('h1', 'acertou!')    
+        exibirTextoNatela('h1', 'acertou!');
+          let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+          let mensagemtentativas = você descobriu o número secreto com ${tentativas}  
     } 
 }
 
