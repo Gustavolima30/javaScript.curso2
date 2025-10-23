a3b913d42413967be25230bb8bf9ddcf890ba87f
Gustavolima30 <lima.munhoz@escola.pr.gov.br>
2025-10-23 11:51:35 -0300

    ok

--- app.js
+++ app.js
@@ -34,8 +34,19 @@ function verificarChute() {
     }
 
     function gerarNumeroAleatorio() {
-        return
+        return parseInt(Math.random() * 3 + 1);
     }
+
+    function limparCampo() {
+        chute = document.querySelector('input');
+        chute.value = '';
     } 
-}
+
+    function reiniciarJogo() {
+        numeroSecreto = gerarNumeroAleatorio();
+        limparCampo();
+        tentativas = 1;
+        exibirMensagemInicial();
+        document.getElementById('reiniciar').setAttribute('disabled', true)
+    }
 
